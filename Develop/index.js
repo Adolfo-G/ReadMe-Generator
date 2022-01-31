6f2387e88d2da395177d3afad9040a934867d451
Adolfo Gonzalez <agonzal177@gmail.com>
2022-01-31 16:18:48 -0500

    added questions and wrote mock up for readme. trying to get init function working

---
+++ Develop/index.js
@@ -0,0 +1,88 @@
+// TODO: Include packages needed for this application
+const inquirer=require("inquirer");
+const fs=require("fs")
+const generate=require("../Develop/utils/generateMarkdown.js");
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'motivation',
+            message: 'What was your motivation?',
+        },
+        {
+            type: 'input',
+            name: 'why',
+            message: 'Why did you build this project?',
+        },
+        {
+            type: 'input',
+            name: 'what',
+            message: 'What problem does it solve?',
+        },
+        {
+            type: 'input',
+            name: 'learn',
+            message: 'What did you learn?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What did you learnWhat are the steps required to install your project ? Provide a step - by - step description of how to get the development environment running?',
+        },
+        {
+            type: 'input',
+            name: 'collab',
+            message: 'Who collaborated in this project?',
+        },
+        {
+            type: 'input',
+            name: '3rdParty',
+            message: 'Where there third party sources?',
+        },
+        {
+            type: 'input',
+            name: 'tutorial',
+            message: 'Any links to tutorials?',
+        },
+        {
+            type: 'input',
+            name: 'instruction',
+            message: 'Provide instructions and examples for use.',
+        },
+        {
+            type: 'input',
+            name: 'photo',
+            message: 'Enter the name of your photo: "example.jpeg"',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'What is the license of your project?',
+        },
+    ]);
+};
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, read) { 
+    fs.writeFile('README.md',user,(err) =>{
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() { 
+    questions()
+        .then((answers) => fs.writeFileSync('index.html', generateHTML(answers)))
+        .then((data)=>generate.generateMarkdown(data))
+}
+
+// Function call to initialize app
+init();
+
+
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,58 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) { }
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) { }
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) { }
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  `# ${data["title"]}
+  ## Description
+
+  - Motivation
+    ${data["motivation"]}
+  - Purpose of project
+    ${data["why"]}
+  - Problem this aims to solve
+    ${data["what"]}
+  - What I learned
+    ${data["learn"]}
+
+  ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+
+  ## Installation
+
+    ${data["installation"]}
+
+  ## Usage
+
+    ${data["instruction"]}
+    ![alt text](assets/images/${data["photo"]})
+
+  ## Credits
+
+  -Collaborators
+    ${data["collab"]}
+
+  -Third Party Assets
+    ${data["3rdParty"]}
+
+  -Tutorials Used
+    ${data["tutorial"]}
+
+  ## License
+    ${data["license"]}`
+    ;
+}
+
+exports.generateMarkdown = generateMarkdown;

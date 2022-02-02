4a276d9ad91bacd2fee1d0c1f8fd9d53bb1a9490
Adolfo Gonzalez <agonzal177@gmail.com>
2022-02-02 03:13:22 -0500

    added conditional parts to readme as well as a badge for license visibility

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  if(license){
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`
+  }else{
+    return ""
+  } 
+ }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  if(license){
+    return `https://opensource.org/licenses/${license}`
+  }else{
+    return ""
+  } 
+ }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+  if(license){
+    return `
+  ## License
+    ${renderLicenseLink(license)}`
+  }else{
+    return ""
+  }
+ }
+
+ function renderCredits(collab,thirdParty,tutorial){
+   if(collab||thirdParty||tutorial){
+     return`
+  ## Credits
+
+  -Collaborators
+    ${collab}
+      
+  -Third Party Assets
+    ${thirdParty}
+      
+  -Tutorials Used
+    ${tutorial}`
+    }else{
+      return ""
+    }
+ }
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown=(data) => {
-  return `# ${data["title"]}
+  return `
+  ${renderLicenseBadge(data["license"])}
+  # ${data["title"]}
   ## Description
 
   - Motivation
@@ -38,26 +78,12 @@ const generateMarkdown=(data) => {
 
     ${data["instruction"]}
     ![alt text](assets/images/${data["photo"]})
-
-  ## Credits
-
-  -Collaborators
-    ${data["collab"]}
-
-  -Third Party Assets
-    ${data["3rdParty"]}
-
-  -Tutorials Used
-    ${data["tutorial"]}
-
-  ## License
-    ${data["license"]}
-  
+ ${renderCredits(data["collab"],data["3rdParty"],data["tutorial"])}
+ ${renderLicenseSection(data["license"])}
   ## Questions
     -For any questions or concerns please reach out to me at
-    github:https://github.com/${github}
-    or
-    email:${email}
+    github:https://github.com/${data["github"]}
+    email:${data["email"]}
     `;
 }
 
